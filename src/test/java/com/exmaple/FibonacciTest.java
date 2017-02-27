397a710f3f9d56d89470f2e44e5ca20b9acf40c0
Marcin Stachniuk <mstachniuk@gmail.com>
2017-02-27 18:01:01 +0100

    2 refactor

--- src/test/java/com/exmaple/FibonacciTest.java
+++ src/test/java/com/exmaple/FibonacciTest.java
@@ -1,32 +1,30 @@
 package com.exmaple;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class FibonacciTest {
 
+    private Fibonacci fibonacci;
+
+    @Before
+    public void setUp() throws Exception {
+        fibonacci = new Fibonacci();
+    }
+
     @Test
     public void shouldReturnZeroForZero() {
-        // given
-        Fibonacci fibonacci = new Fibonacci();
-
-        // when
         int result = fibonacci.fibonacci(0);
 
-        // then
         assertEquals(0, result);
     }
 
     @Test
     public void shouldReturnOneForOne() {
-        // given
-        Fibonacci fibonacci = new Fibonacci();
-
-        // when
         int result = fibonacci.fibonacci(1);
 
-        // then
         assertEquals(1, result);
     }
 }
