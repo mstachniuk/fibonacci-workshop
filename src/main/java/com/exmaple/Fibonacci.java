ed1fa4d8d2832971a5d04a02ed8c794426d690d0
Marcin Stachniuk <mstachniuk@gmail.com>
2017-02-27 18:22:08 +0100

    5 refactor 3

--- src/main/java/com/exmaple/Fibonacci.java
+++ src/main/java/com/exmaple/Fibonacci.java
@@ -5,6 +5,14 @@ public class Fibonacci {
         if(n < 2) {
             return n;
         }
-        return fibonacci(n-1) + fibonacci(n-2);
+        int before1 = 1;
+        int before2 = 0;
+        int sum = 0;
+        for (int i = 2; i <= n; i++) {
+            sum = before1 + before2;
+            before2 = before1;
+            before1 = sum;
+        }
+        return sum;
     }
 }
