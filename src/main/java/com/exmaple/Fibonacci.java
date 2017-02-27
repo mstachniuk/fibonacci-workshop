8a264673601a889ca67135327253bff2b90ba808
Marcin Stachniuk <mstachniuk@gmail.com>
2017-02-27 18:05:31 +0100

    4 green

--- src/main/java/com/exmaple/Fibonacci.java
+++ src/main/java/com/exmaple/Fibonacci.java
@@ -2,8 +2,8 @@ package com.exmaple;
 
 public class Fibonacci {
     public int fibonacci(int n) {
-        if(n == 2) {
-            return 1;
+        if(n > 1) {
+            return n - 1;
         }
         return n;
     }
