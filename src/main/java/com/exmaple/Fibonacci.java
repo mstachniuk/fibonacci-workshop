cf127d1497349780354f6ad2a02c00d033a3d3d0
Marcin Stachniuk <mstachniuk@gmail.com>
2017-02-27 18:14:24 +0100

    5 refactor 2

--- src/main/java/com/exmaple/Fibonacci.java
+++ src/main/java/com/exmaple/Fibonacci.java
@@ -2,7 +2,7 @@ package com.exmaple;
 
 public class Fibonacci {
     public int fibonacci(int n) {
-        if(n == 0 || n == 1) {
+        if(n < 2) {
             return n;
         }
         return fibonacci(n-1) + fibonacci(n-2);
